9cd2eb61fd622f24b5d72549f1cb31ff0b0c3226
Sandi Macek <sandi.macek@gmail.com>
2015-11-17 01:38:30 +0000

    Added dynamic menu and created redirection if slug is incorrect

--- src/FizzBuzz/Controller/Homepage.php
+++ src/FizzBuzz/Controller/Homepage.php
@@ -4,8 +4,18 @@ namespace FizzBuzz\Controller;
 
 class Homepage extends AbstractController
 {
+    public function menuItems()
+    {
+        $sectionsRepository = $this->app->container->get('SectionsRepository');
+        $menuItems = $sectionsRepository->viewAllSections();
+        $this->tpl->menuItems = $menuItems;
+    }
+
     public function homepage()
     {
+        //call menu on homepage
+        $this->menuItems();
+        
         // We display articles from the news section on the homepage
         $articlesRepository = $this->app->container->get('ArticlesRepository');
         $articles = $articlesRepository->findAll(['section_id' => 1]);
@@ -17,6 +27,7 @@ class Homepage extends AbstractController
         }
 
         echo $this->tpl->render('homepage.phtml');
+
     }
 
     public function phpinfo()
@@ -26,10 +37,15 @@ class Homepage extends AbstractController
 
     public function handle404()
     {
+        //display menu items even on 404 page otherwise we get an error. 
+        $this->menuItems();
+
         $this->app->response->setStatusCode(404);
         
         $this->tpl->uri = $this->getRoutedParam('uri');
 
         echo $this->tpl->render('404.phtml');
     }
+
+
 }
